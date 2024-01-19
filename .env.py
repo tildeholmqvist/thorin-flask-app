b500e3c1a33ec38b20aa312a3e8abd6a6a23e4ae
Tilde Holmqvist <tildeholmqvist@gmail.com>
2024-01-19 11:00:36 +0000

    added gitignore

---
+++ .env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ.setDefault("SECRET_KEY", "secret_flash_key")
--- run.py
+++ run.py
@@ -1,32 +1,46 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash 
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def index ():
+def index():
     return render_template("index.html")
 
 
 @app.route("/about")
-def about ():
+def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_titel="About", company=data)
+    return render_template("about.html", page_title="About", company=data)
 
 
-@app.route("/contact")
-def contact ():
-    return render_template("contact.html", page_titel="Contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
-def careers ():
-    return render_template("careers.html", page_titel="Careers")
+def careers():
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
